0df10bd49c6ae8135f04fe8ca0345901673032a6
sejin Park <sejinp98@gmail.com>
2023-02-28 13:09:28 +0900

    organize

---
+++ model/Analysis.py
@@ -0,0 +1,55 @@
+from Survival_CostFunc_CIndex import *
+import torch
+from DataLoader import load_data
+from Model import test_model
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+
+def draw_learning-rate(train_cost, val_cost, name) :
+    #train_cost = train_cost.numpy()
+    train_cost = torch.tensor(train_cost).detach().numpy()
+    # #val_cost = val_cost.numpy()
+    val_cost = torch.tensor(val_cost).detach().numpy()
+    
+    plt.plot(train_cost)
+    plt.plot(val_cost)
+    plt.xlabel('epoch')
+    plt.ylabel('cost')
+    plt.legend(['train', 'val'])    
+    plt.savefig('LearningCurve1/' + name + '.png')
+    plt.clf()
+    
+def createCIndexArray() :
+    dtype = torch.FloatTensor
+    X_test, t_test, e_test = load_data('./test_data.csv', dtype)
+    cindexs = []
+    
+    for (root, directories, files) in os.walk('./models') :
+        for file in files :
+            file_path = os.path.join(root, file)
+            activation = file.split('_')[1]
+            drop_out = file.split('_')[-3]
+            model = test_model(len(X_test[0]), activation, float(drop_out))
+            model.load_state_dict(torch.load('./models/' + file))
+            model.eval()
+    
+            pred = model(X_test)
+            test_cindex = c_index(pred, t_test, e_test)
+            cindexs.append(test_cindex)
+            
+    plt.boxplot(cindexs.detach().numpy())
+    plt.xlabel('type')
+    plt.ylabel('value')   
+    plt.savefig("./hidden2_boxplot.png")
+    plt.clf()
+   
+def calculateCIndex(cindexes, name) :
+    plt.boxplot(cindexes)
+    plt.xlabel('type')
+    plt.ylabel('value')   
+    plt.savefig('./Analysis/' + name + '.png')
+    plt.clf()
+            
+createCIndexArray()
---
+++ model/Run.py
@@ -0,0 +1,44 @@
+import os
+import pandas as pd
+import numpy as np
+#from Preprocessing import *
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from Train import train
+from DataLoader import *
+import torch
+from Analysis import draw_learning
+import matplotlib.pyplot as plt
+
+#os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+#os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
+def run() :    
+    learning_rate = [0.9]
+    regularization = [0.7]
+    act = ["Tanh"]
+    dropout_rates = [0.1]
+    optimizers = ["NAdam"]
+    
+    lr = 0.9
+    r2 = 0.7
+    a = "Tanh"
+    dropout_rate = 0.1
+    optimizer = "NADam"
+    
+    epochs = 1000
+    dtype = torch.FloatTensor
+    
+    X_train, t_train, e_train = load_data('./train_data.csv', dtype)
+    X_test, t_test, e_test = load_data('./test_data.csv', dtype)
+    X_val, t_val, e_val = load_data('./val_data.csv', dtype)
+    
+    #for a in act :
+        #for lr in learning_rate:
+            #for r2 in regularization:
+                #for dropout_rate in dropout_rates :
+                    #for optimizer in optimizers :
+    train_cost, val_cost, test_cindex = train(X_train, t_train, e_train, X_val, t_val, e_val, X_test, t_test, e_test, a, lr, r2, epochs, dropout_rate, optimizer)
+    draw_learning(train_cost, val_cost, str(a) + '_' + str(lr) + '_' + str(l2) + '_' + str(dropout_rate) + '_' + str(optimizer))
+                        
+    return test_cindex, model
